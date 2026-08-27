46832e701b424696a9c99c1db1e18ef9d2ff70f9
KrishmalX <krishmaldinidu5466@gmail.com>
2026-08-28 01:48:20 +0530

    Patch: Fix the console errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
 import PreviewPage from "@/pages/PreviewPage";
 import AppLayout from "@/components/AppLayout";
 
 export default function Page() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <BrowserRouter>
       <AppLayout>
